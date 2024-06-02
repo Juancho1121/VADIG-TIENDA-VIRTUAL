f316ee2948a6be6a421f98642647a3641d0c4111
Juancho1121 <juanchod456@gmail.com>
2024-06-02 18:26:47 -0500

    cambios

--- src/main/java/com/vadigaccesorios/vadig/ResourceWebConfiguration.java
+++ src/main/java/com/vadigaccesorios/vadig/ResourceWebConfiguration.java
@@ -1,6 +1,5 @@
 package com.vadigaccesorios.vadig;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
